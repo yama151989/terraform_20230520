311fe915d985571c6b5ceaa82408b7f735668d32
香川 直希 <yama151989@gmail.com>
2023-05-20 16:02:22 +0900

    一旦コミット

--- main.tf
+++ main.tf
@@ -1,10 +1,24 @@
-provider "aws" {
-    # AWSCLIの初期設定で指定したプロファイル。このIAMユーザーの情報で接続する。
-    profile = "terraform"
-    region = "ap-northeast-1"
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
 }
 
-resource "aws_instance" "test_ec2" {
-  ami = "ami-0e0820ad173f20fbb"
-  instance_type = "t2.micro"
+provider "aws" {
+  # AWSCLIの初期設定で指定したプロファイル。このIAMユーザーの情報で接続する。
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+# 変数を2つ定義する。値は、「terraform.tfvars」で指定。
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
 }
